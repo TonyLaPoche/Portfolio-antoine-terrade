d9177a206f90b26741ca5258166c785821386189
tonylapoche <terrade.antoine.pro@gmail.com>
2023-03-08 16:36:02 +0100

    save WP Nav

--- my-app/src/component/header/nav/index.js
+++ my-app/src/component/header/nav/index.js
@@ -2,50 +2,186 @@ import React, { useState } from "react";
 import useWindowSize from "../../../hooks";
 import { HashLink } from 'react-router-hash-link';
 
-export default function SideNav({...props}) {
+import fakeProps from "../../../data/fake";
+import { Link, useLocation } from "react-router-dom";
+
+export default function SideNav({ ...props }) {
   const [slider, setSlider] = useState(false);
+  const [subSlider, setSubSlider] = useState(false);
   const size = useWindowSize();
 
-  console.log(props);
-console.log('url courante : ',window.location.hash);
-const { location } = window
+  const location = useLocation();
   const { navElements } = props
+
+  const nav = fakeProps.slice(1)
+  console.groupCollapsed("nav")
+  console.log(nav)
+  console.groupEnd()
+  console.log(window);
+
+  // TODO - A déplacé
+  const isActiveLink = (link) => {
+    return location.pathname === `/${link}` ? 'red-text' : 'waves-effect ';
+  }
+
+
+
+  const navGenerator = () => {
+    const NavElt = nav.map((elt) => {
+      if (elt.name === "présentation" || elt.name === "mes réseaux" || elt.name === "contact") {
+        return (
+          <li key={elt.name} onClick={() => setSlider(s => !s)}>
+            <HashLink
+              key={'nav-' + elt.id + elt.name}
+              className={`${location.hash}` === elt.path ? "waves-effect red-text" : "waves-effect"}
+              to={'/' + elt.path}
+            >
+              {elt.name}
+            </HashLink>
+          </li>
+          )
+      }
+      if (elt.name === "mes créations") {
+        return (
+          <li key={elt.name}>
+          <ul key={elt.name + "-collapsible-accordion"} className="collapsible collapsible-accordion">
+            <li key={elt.name + "-collapsible-header"}>
+              <HashLink
+                key={elt.name + "-collapsible-header-link"}
+                className="collapsible"
+                to="/#projets"
+                onClick={() => setSubSlider(s => !s)}
+              >
+                {elt.name}
+                <i key={elt.name + "-collapsible-projets-link-icon"} className="material-icons right">
+                {
+                  subSlider ? 'expand_more' :'expand_less'
+                }
+                
+                </i>
+              </HashLink>
+              
+              <div
+                key={elt.name + "-collapsible-body"}
+                className="collapsible-body"
+                style={{
+                  display: subSlider ? "block" : "none"
+                }}
+              >
+                <ul key={elt.name + "-callapsible-body-ul"}>
+                  {elt.content.map(content => (
+                    <li key={"li" + content.id}>
+                      <Link
+                        key={content.id + content.title}
+                        to={content.link}
+                        style={{marginLeft: "15px", fontSize:"1rem"}}
+                        className={`${isActiveLink(content.link)} animate__animated animate__backInLeft`}
+                      >
+                         <i key={'icon-' + content.id + content.title} className="material-icons" style={{marginRight: "15px"}}>arrow_forward</i> {content.title}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </li>
+          </ul>
+        </li>
+          )
+      }
+      return null
+    })
+
+    return NavElt
+  }
+
+  const elementsJSX = navGenerator()
+
+  console.log()
+
+  // console.groupCollapsed("présentation");
+  // console.log(fakeProps[1]);
+  // console.groupEnd();
+
+  // console.groupCollapsed("mes créations");
+  // console.log(fakeProps[2]);
+  // console.groupEnd();
+
+  // console.groupCollapsed("Mes réseaux");
+  // console.log(fakeProps[3]);
+  // console.groupEnd();
+
+  // console.groupCollapsed("contact");
+  // console.log(fakeProps[4]);
+  // console.groupEnd();
 
   return (
     <>
-      <nav className="nav-wrapper light-blue">
-        <p>Porte folio du Tony</p>
-        <div
-          className="btn-floating btn-large waves-effect waves-light red"  
-          onClick={() => setSlider(s => !s)}
-        >
-          <i className="material-icons">menu</i>
+      <nav>
+        <div className="nav-wrapper light-blue">
+          <a className="brand-logo center" href="/">{fakeProps[0].titre}</a>
+          <div
+            className="btn-floating btn-small blue darken-2 center-left pulse"
+            style={{ marginLeft: '15px' }}
+            onClick={() => setSlider(s => !s)}
+          >
+            <i className="material-icons">menu</i>
+          </div>
+
         </div>
       </nav>
       <div
         className="sidenav-overlay"
         onClick={() => setSlider(s => !s)}
         style={{
-          display: slider && size.width < 600 ? "block" : "none",
-          opacity: "1"
+          display: slider && size.width > 250 ? "block" : "none",
+          opacity: ".5"
         }}
       />
       <ul
         id="slide-out"
         className="sidenav"
         style={{
-          transform: slider || size.width > 600 ? "translateX(0%)" : "",
+          transform: slider && size.width > 250 ? "translateX(0%)" : "",
           transitionProperty: "transform",
-          transitionDuration: ".75s"
+          transitionDuration: "500ms"
         }}
       >
         <li>
-          <img src="https://fakeimg.pl/100/" alt="fakeimg" />
-          <h4>Terrade Antoine</h4>
+          <div className="user-view">
+            <div className="background">
+              <img src="	https://materializecss.com/images/office.jpg" alt="background sidenav" />
+            </div>
+            <div
+              className="btn-floating btn-small blue darken-2 right pulse"
+              // style={{ marginLeft: '15px' }}
+              onClick={() => setSlider(false)}
+            >
+              <i className="material-icons">close</i>
+            </div>
+            <a href="#user" >
+              <img src="https://fakeimg.pl/220/" alt="img profil tony" className="circle responsive-img" style={{ margin: "0 auto" }} />
+            </a>
+            <a href="#presentation">
+              <span className="white-text name">Tony La Poche</span>
+            </a>
+            <a href="#title">
+              <span className="white-text name">Développeur Web Full-stack</span>
+            </a>
+          </div>
         </li>
-        {navElements.map(elt => (
+
+        <li>
+          <a href="/" className="title" onClick={() => setSlider(s => !s)} >
+            <span className="center">Accueil</span>
+          </a>
+        </li>
+        <li>
+          <div className="divider"></div>
+        </li>
+        {elementsJSX}
+        {/* {navElements.map(elt => (
           <li key={elt.name} onClick={() => setSlider(s => !s)}>
-            <HashLink 
+            <HashLink
               key={elt.id}
               className={`${location.hash}` === elt.path ? "waves-effect red-text" : "waves-effect"}
               to={elt.path}
@@ -54,7 +190,40 @@ const { location } = window
             </HashLink>
           </li>
         ))}
+
+        <li>
+          <ul className="collapsible collapsible-accordion">
+            <li>
+              <a className="collapsible" href="#projets" onClick={() => setSubSlider(s => !s)}>
+                Mes Créations
+                <i className="material-icons right">arrow_drop_down</i>
+              </a>
+              <div
+                className="collapsible-body"
+                style={{
+                  display: subSlider ? "block" : "none"
+                }}
+              >
+                <ul>
+                  <li><a href="#!">Projet 1</a></li>
+                  <li><a href="#!">Projet 2</a></li>
+                  <li><a href="#!">Projet 3</a></li>
+                  <li><a href="#!">Projet 4</a></li>
+                </ul>
+              </div>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <div className="divider"></div>
+        </li>
+        <li>
+          <a className="waves-effect" href="#contact">
+            contact
+          </a>
+        </li> */}
       </ul>
+      
     </>
   );
 }
--- my-app/src/hooks.js
+++ my-app/src/hooks.js
@@ -1,14 +1,14 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 
 export default function useWindowSize() {
   const isClient = typeof window === "object";
 
-  function getSize() {
+  const getSize = useCallback(() => {
     return {
       width: isClient ? window.innerWidth : undefined,
       height: isClient ? window.innerHeight : undefined
     };
-  }
+  }, [isClient]);
 
   const [windowSize, setWindowSize] = useState(getSize);
 
@@ -17,13 +17,13 @@ export default function useWindowSize() {
       return false;
     }
 
-    function handleResize() {
+    const handleResize = () => {
       setWindowSize(getSize());
-    }
+    };
 
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
-  }, []); // Empty array ensures that effect is only run on mount and unmount
+  }, [getSize, isClient]);
 
   return windowSize;
 }
