12940614ed7f9b55f576b07644d16ccfe0ae2ecf
tonylapoche <terrade.antoine.pro@gmail.com>
2023-03-17 12:11:42 +0100

    transition react-materialize -> tailwindCss

--- my-app/src/view/Project/index.js
+++ my-app/src/view/Project/index.js
@@ -1,54 +1,45 @@
 // == Import
 // import PropTypes from 'prop-types';
-import Techno from './techno';
-import fakeProps from '../../data/fake.js';
 import { useLocation } from 'react-router-dom';
-import { Card, CardTitle, Col, Row } from 'react-materialize';
+import fakeProps from '../../data/fake.js';
+import Techno from './techno';
 import BadgeM from './badge';
-import { isMobile } from 'react-device-detect';
 
-// console.log(fakeProps);
 const projects = fakeProps[2].content;
 // == Composant
 const Project = () => {
   const location = useLocation();
   const slug = location.pathname.split('/').pop();
   const data = projects.filter((proj) => proj.id === slug)[0];
-  //   console.log(data.techno);
 
   return (
-    <div id="projects" className="container">
-      <h2 className="center">MON PROJET DE TEST</h2>
-
-      <div className="center">
+    <div id="projects" className="container md:pt-5">
+      <h2 className="center text-3xl my-3 uppercase">{data.title}</h2>
+      <div className="flex justify-center">
         {data.techno.map((t) => (
           <Techno key={t} techno={t} />
         ))}
       </div>
-      <Row>
-        <Col m={12} s={12}>
-          <Card
-            horizontal={!isMobile}
-            actions={[
-              <a key="repos" href={data.link} target="_blank" rel="noreferrer">
-                Repos
-              </a>,
-              <a key="demo" href={data.demo} target="_blank" rel="noreferrer">
-                Démo
-              </a>,
-            ]}
-            header={
-              <CardTitle image={data.picture}>
-                <BadgeM etat={'terminé'} />
-              </CardTitle>
-            }
-          >
-            {/* {data.description} */}
-							{data.description.map(speech => <p>{speech}</p>)}
+      <div className='mx-auto mb-5'>
+        <div  className="w-full " >
+          <div className='relative flex flex-col md:flex-row m-4 border-2 border-solid'>
+            <div className='relative md:w-full' >
+              <img src={data.picture} alt={data.alt} className='w-full' />
+              <BadgeM etat={data.etat}/>
+            </div>
+            <div className='p-4 flex flex-col justify-between divide-y'>
+              <div className='leading-5'>
+                {data.description.map(speech => <p className='mb-2 text-justify'>{speech}</p>)}
+              </div>
+              <div className='flex justify-evenly pt-2'>
+                <a href={data.link} className='text-blue-700 hover:text-blue-500 text-xl hover:underline underline-offset-1' target="_blank" rel='noreferrer'> Repos</a>
+                <a href={data.demo} className='text-blue-700 hover:text-blue-500 text-xl hover:underline underline-offset-1'target="_blank" rel='noreferrer'> Démo</a>
+              </div>
+            </div>
 
-          </Card>
-        </Col>
-      </Row>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
